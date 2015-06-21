7af877ce3dc65cf30f221d4f1baf549c140123cc
Lysandro Carioca Braga <lysandroc@gmail.com>
2015-06-21 15:20:42 -0300

    Maneira que CommonJs funciona!

---
+++ app.js
@@ -0,0 +1,8 @@
+
+//unica funcao modular
+var hello = require('./hello');
+//varias funcoes modulares
+var human = require('./human');
+
+hello('Olá pessoal!');
+human.hello('Olá galera!');
---
+++ human.js
@@ -0,0 +1,12 @@
+//Funcoes modulares
+exports.hello = function(msg) {
+    console.log(msg);
+};
+
+exports.hello1 = function(msg) {
+    console.log(msg);
+};
+
+exports.hello2 = function(msg) {
+    console.log(msg);
+};
