134d42c5cd58ae1eeeae22b94dde07c29a9d39ab
Lysandro Carioca Braga <lysandroc@gmail.com>
2015-06-22 22:36:50 -0300

    Melhoria no desafio, verificar se existe o arquivo artigo.html e refatoracao

--- server_rotas.js
+++ server_rotas.js
@@ -3,20 +3,29 @@ var url = require("url");
 var fs = require("fs");
 
 http.createServer(function(request, response) {
-	var endereco_arquivo;
-	if (request.url == "/artigos" || request.url == "/artigos") {
-		endereco_arquivo = '/artigos.html';
-	} else if(request.url == "/contato") {
-		endereco_arquivo = '/contato.html';
-	} else {
-		endereco_arquivo = '/erro.html';	
-	}
-	fs.readFile(__dirname + endereco_arquivo, function(err,html) {
+	var endereco_artigo = '/artigos.html', 
+		endereco_contato = '/contato.html', 
+		endereco_erro = '/erro.html', 
+		result = url.parse(request.url);
+	
+	function EscreveHtmlResposta(nomeArquivo) { 
+		fs.readFile(__dirname + nomeArquivo, function(err,html) {
 		response.writeHead(200, {'Content-Type': 'text/html'});
 		response.write(html);
 		response.end();
-	});		
+		});
+	}
+	
+	fs.exists(endereco_artigo.substr(1,endereco_artigo.length), function(exists) {
+		if (exists && result.path == "/artigos" || result.pathname == "") {
+			EscreveHtmlResposta(endereco_artigo);
+		} else if(result.pathname == "/contato") {
+			EscreveHtmlResposta(endereco_contato);
+		} else {
+			EscreveHtmlResposta(endereco_erro);
+		}
+	});
 	
 }).listen(3000, function() {
 	console.log('Aplicacao inicializada!');
-});	 
+});
